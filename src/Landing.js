6f4a23b212587a3ef334a7fc56bcf800ea1ed845
Sloane <git@sloaney.ca>
2024-06-30 23:30:50 -0700

    added landing page

--- src/Landing.js
+++ src/Landing.js
@@ -1,10 +1,23 @@
 
 function Landing() {
     return (
-        <header class="landing">
+        <>
+        {/* to do: align left-aligned text block to center,
+        make compatible with different browser sizes */}
+        <head>
+        <style>
+            @import url('https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap');
+            </style>
+        </head>
+        <header class="landing container">
+
             <h1 class="landing-text">Hello, I am</h1>
-            <h1 class="landing-title">Sloane Wensauer</h1>
+            <div class="center">
+                <h1 class="landing-title">Sloane Wensauer</h1>
+            </div>
         </header>
+        </>
+
     );
 }
 
